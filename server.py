47d6d9f92284305a37aa36f2908a856a213f4715
Lee <leeuklee@gmail.com>
2023-04-23 23:21:30 +0200

    updated docker container

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import schedule
 import time
 
 from ssh_commands import run_command_via_ssh
-from env import user, password, host, reboot_command
+from env import host, user, password, reboot_command
 
 
 schedule.every().day.at("12:00", "Europe/Amsterdam").do(run_command_via_ssh(host, user, password, reboot_command))
